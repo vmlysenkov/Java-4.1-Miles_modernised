01ccdea05b11d1abbaadbd20977e18ef7c80191b
Vasilii Lysenkov <lysenkov2@ya.ru>
2021-08-22 11:46:09 +0300

    Updated code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 99985;
-        int miles = service.calculate(price);
+
+        int miles = service.calculate(99985);
         System.out.println("Накопленные мили:" +miles);
     }
 }
